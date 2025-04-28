ddd1f833414dcd1f5d8c2f64e6c9178cd01a65c6
Akhilesh Bonde <110801340+akhileshh-b@users.noreply.github.com>
2025-04-28 09:00:00 +0530

    Time: 0 ms (100%), Space: 17.4 MB (86.08%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,43 +1,46 @@
 class Solution {
 public:
+    int firstocc(vector<int>& nums, int target){
+        int low = 0;
+        int high = nums.size() - 1;
+        int ans=-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                ans = mid;
+                high = mid -1;
+            }
+            else if(nums[mid]<target){
+                low = mid +1;
+            }
+            else if(nums[mid]>target){
+                high = mid -1;
+            }
+        }
+        return ans;
+    }
+    int lastocc(vector<int>& nums, int target){
+        int low = 0;
+        int high = nums.size() - 1;
+        int ans=-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                ans = mid;
+                low = mid +1;
+            }
+            else if(nums[mid]<target){
+                low = mid +1;
+            }
+            else if(nums[mid]>target){
+                high = mid -1;
+            }
+        }
+        return ans;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n=nums.size();
-        int l=0;
-        int h=n-1;
-        int ans1=INT_MAX;
-        int ans2=INT_MIN;
-        while(l<=h){
-            int mid=l+(h-l)/2;
-          
-            if(nums[mid]==target){
-                ans1=min(ans1,mid);
-                h=mid-1;
-            }
-            else if(nums[mid]>target){
-                h=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        l=0;
-        h=n-1;
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            if(nums[mid]==target){
-                ans2=max(ans2,mid);
-                l=mid+1;
-            }
-            else if(nums[mid]>target){
-                h=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        if(ans1==INT_MAX){
-            return {-1,-1};
-        }
-        return {ans1,ans2};
+        
+        vector<int> v = {firstocc(nums,target),lastocc(nums,target)};
+        return v;
     }
 };
